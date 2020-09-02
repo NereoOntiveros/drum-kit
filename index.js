40324d97ef37f157ddc81884c5632fae5591b8e4
Nereo Ontiveros <nerontiveros@gmail.com>
2020-09-02 09:44:11 +0100

    Add event listeners

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-alert("hole");
+var drumButtonList = document.querySelectorAll(".drum");
+
+drumButtonList.forEach(item=>{
+  item.addEventListener("click",event=>{
+    alert("I got clicked!!");
+  })
+})
